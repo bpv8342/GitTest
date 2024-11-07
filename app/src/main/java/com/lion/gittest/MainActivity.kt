29f8cce449f307074b4f9b9c5cf8238f5fd3622d
bpv834 <bpv8342Gmail.com>
2024-11-07 11:26:22 +0900

    commit1

--- app/src/main/java/com/lion/gittest/MainActivity.kt
+++ app/src/main/java/com/lion/gittest/MainActivity.kt
@@ -7,6 +7,7 @@ import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
+    val a= 1
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -15,6 +16,7 @@ class MainActivity : AppCompatActivity() {
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+
         }
     }
 }
